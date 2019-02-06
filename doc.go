378ec4f38c3e7070ec6b319d60fc1afdf5748221
konimarti <koni.marti@gmail.com>
2019-02-06 16:32:25 +0100

    Documentation

--- doc.go
+++ doc.go
@@ -7,9 +7,9 @@
 //
 // where x(t) represents the state and u(t) the control input vectors and the
 // matrices are
-// 	A: System matrix,
-//      B: Control matrix,
-//      C: Output matrix and
-//      D: Feedforward matrix
+// A: System matrix,
+// B: Control matrix,
+// C: Output matrix and
+// D: Feedforward matrix
 //
 package lti
